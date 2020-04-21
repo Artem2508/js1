9c1b8a0148c8615fbf76fddb6a5e69100ed40b56
Artem Saidakov <63412183+Artem2508@users.noreply.github.com>
2020-04-21 14:34:21 +0200

    Add files via upload

---
+++ js-basics-task1.js
@@ -0,0 +1,11 @@
+
+
+const yourName = 'Artem Saidakov';
+const birthDate = '25.08.2000';
+var num1 = '1';
+var num2 = '8';
+var num3 = '1';
+
+console.log(`Hello! This program has been written by ${yourName}`);
+console.log(`Author has been born on ${birthDate}`);
+console.log(`The result of hidden equation is: ${(num1 + num2) * num3}`);
